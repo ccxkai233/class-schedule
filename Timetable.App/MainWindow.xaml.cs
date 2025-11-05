62e58a66d95f4f242d5bc099c54ffb006dd796a9
ccxkai233 <2658628491@qq.com>
2025-11-05 13:35:01 +0800

    Feat: Implement close/minimize to tray and add context menu

--- Timetable.App/MainWindow.xaml.cs
+++ Timetable.App/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Input;
 using Timetable.App.ViewModels;
+using Forms = System.Windows.Forms;
 
 namespace Timetable.App;
 
@@ -9,10 +12,78 @@ namespace Timetable.App;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly Forms.NotifyIcon _notifyIcon;
+    private bool _isExplicitClose;
+
     public MainWindow()
     {
         InitializeComponent();
         // DataContext is set in XAML
+
+        _notifyIcon = new Forms.NotifyIcon();
+        var iconStream = Application.GetResourceStream(new Uri("pack://application:,,,/Resources/icon.ico"))?.Stream;
+        if (iconStream != null)
+        {
+            _notifyIcon.Icon = new System.Drawing.Icon(iconStream);
+        }
+        _notifyIcon.Text = "电子课程表";
+        _notifyIcon.MouseClick += NotifyIcon_MouseClick;
+
+        var contextMenu = new Forms.ContextMenuStrip();
+        contextMenu.Items.Add("显示", null, ShowWindow_Click);
+        contextMenu.Items.Add("退出", null, Exit_Click);
+        _notifyIcon.ContextMenuStrip = contextMenu;
+
+        StateChanged += MainWindow_StateChanged;
+    }
+
+    protected override void OnClosing(CancelEventArgs e)
+    {
+        if (!_isExplicitClose)
+        {
+            e.Cancel = true;
+            Hide();
+            _notifyIcon.Visible = true;
+        }
+        base.OnClosing(e);
+    }
+
+    private void Exit_Click(object? sender, EventArgs e)
+    {
+        _isExplicitClose = true;
+        _notifyIcon.Dispose();
+        Close();
+    }
+
+    private void ShowWindow_Click(object? sender, EventArgs e)
+    {
+        ShowAndActivate();
+    }
+
+    private void MainWindow_StateChanged(object? sender, EventArgs e)
+    {
+        if (WindowState == WindowState.Minimized)
+        {
+            Hide();
+            _notifyIcon.Visible = true;
+        }
+    }
+
+    private void NotifyIcon_MouseClick(object? sender, Forms.MouseEventArgs e)
+    {
+        if (e.Button == Forms.MouseButtons.Left)
+        {
+            // A simple left-click on the icon will also show the window.
+            ShowAndActivate();
+        }
+    }
+
+    private void ShowAndActivate()
+    {
+        Show();
+        WindowState = WindowState.Normal;
+        Activate();
+        _notifyIcon.Visible = false;
     }
 
     private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
